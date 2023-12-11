4a3956017e4a3a6a3113f5473817ba3bd37adfd7
BeansMage42 <144922891+BeansMage42@users.noreply.github.com>
2023-12-11 14:33:33 -0500

    updated dodging
    updated dodging

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,27 @@ public class Player : MonoBehaviour
     [SerializeField] float _moveSpeed;
     [SerializeField] float _dodgeSpeed;
     [SerializeField] float encumberence;
+    private bool isDodging;
+    private bool isAttacking;
+    private bool iFrameActive;
+    private WaitForSeconds dodgeWait;
+    private WaitForSeconds dodgeInvulnerable;
 
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         Controls.Init(this);
-        rb = GetComponent<Rigidbody2D>();
+        dodgeWait = new WaitForSeconds(0.2f);
     }
 
     void FixedUpdate()
     {
         //rb.velocity = _moveDir * _moveSpeed;
-
+        if (isDodging || isAttacking) return;
         rb.AddForce(_moveDir * _moveSpeed * encumberence , ForceMode2D.Impulse);
     }
 
@@ -30,6 +40,17 @@ public class Player : MonoBehaviour
     public void Dodge()
     {
         Debug.Log("Dodging...");
+        if (isDodging) return;
+        StartCoroutine("DodgeCoolDown");
         rb.AddForce(_moveDir * _dodgeSpeed, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator DodgeCoolDown()
+    {
+        isDodging = true;
+        yield return dodgeWait;
+        isDodging = false;
+
+
     }
 }
