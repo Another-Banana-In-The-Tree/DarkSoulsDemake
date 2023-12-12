caf44b9af0a1ca08ed666d85448f324d63332ec8
BeansMage42 <144922891+BeansMage42@users.noreply.github.com>
2023-12-12 11:30:49 -0500

    added stats

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //STATS
+    [field: Header("Stats")]
+    [SerializeField] private float healthMax;
+    [SerializeField] private float healthTotal;
+    [SerializeField] private float staminaMax;
+    [SerializeField] private float staminaTotal;
+    [SerializeField] private float vitality;
+    [SerializeField] private float attunement;
+    [SerializeField] private float endurance;
+    [SerializeField] private float strength;
+    [SerializeField] private float dexterity;
+    [SerializeField] private float resistance;
+    [SerializeField] private float intelligence;
+    [SerializeField] private float faith;
+    [SerializeField] private float humanity;
+
+
+
+    //MOVEMENT
     Rigidbody2D rb;
     Vector2 _moveDir;
     [field: Header("Speed")]
-    [SerializeField] float _moveSpeed;
-    [SerializeField] float _dodgeSpeed;
-    [SerializeField] float encumberence;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _dodgeSpeed;
+    [SerializeField] private float encumberence;
+    //INPUT CHECKS
     private bool isDodging;
     private bool isAttacking;
     private bool iFrameActive;
+    //TIMERS
     [field: Header("Delays")]
     [SerializeField] private float dodgeDelay;
     [SerializeField] private float dodgeIFrames;
@@ -76,5 +97,15 @@ public class Player : MonoBehaviour
     private IEnumerator AttackCoolDown(WaitForSeconds delay)
     {
         yield return delay;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthTotal -= damage;
+    }
+
+    public void ReduceStamina(float usedStamina)
+    {
+        staminaTotal -= usedStamina;
     }
 }
